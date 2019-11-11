0acdbd498c7880501e997ce2ac53be40ece4ab63
Andrey Kislyuk <kislyuk@gmail.com>
2019-11-10 23:04:56 -0800

    Bump aurora-data-api dependency minimum version to 0.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=open('README.rst').read(),
     install_requires=[
         'sqlalchemy',
-        'aurora-data-api >= 0.1.0'
+        'aurora-data-api >= 0.1.2'
     ],
     extras_require={
     },
