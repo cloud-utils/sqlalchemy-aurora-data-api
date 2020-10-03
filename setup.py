f8c3cf3add2ef96fff7d533905f2bcee916fcf79
Andrey Kislyuk <kislyuk@gmail.com>
2020-10-03 08:15:20 -0700

    Bump dependency version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=open('README.rst').read(),
     install_requires=[
         'sqlalchemy',
-        'aurora-data-api >= 0.2.4'
+        'aurora-data-api >= 0.2.5'
     ],
     extras_require={
     },
